1fa2ac53673e217e8ecf82004ac0f921a1039f94
Ederlo Rodrigo de Oliveira <ederlo.contato@gmail.com>
2017-12-14 10:01:33 -0200

    add 6180 Pimaco tag format

--- test/index.test.js
+++ test/index.test.js
@@ -5,11 +5,51 @@ var fs = require('fs');
 
 var data = require('./names.json');
 
-describe('makeTickets', function () {
+describe('makeTickets6187', function () {
   it('it should make 85 tickets', function(done) {
     var pt = new PrintTicket('pimaco_6187');
 
-    pt.doc.pipe( fs.createWriteStream('test/results/output.pdf') );
+    pt.doc.pipe( fs.createWriteStream('test/results/6187.pdf') );
+    pt.doc.fontSize(6);
+
+    pt.makeTickets({
+      count: data.length
+    }, function (i, marginLeft, marginTop, size, next) {
+
+      pt.doc.text(data[i].name, marginLeft,  marginTop, size)
+      // ticket box
+      pt.doc.lineWidth(0.3);
+
+      pt.doc.rect(marginLeft, marginTop, size.width, size.height).stroke();
+
+      next();
+    }, function(){
+      pt.doc.end();
+    });
+
+    pt.doc.on('end', function(){
+      done();
+    })
+  });
+
+  it('it should throw error if not find the format', function(done) {
+    var p;
+    try {
+      p = new PrintTicket();
+    } catch(e) {
+      assert(!p);
+      assert(e);
+      assert.equal(e.message, 'Unavaible or invalid tag page format');
+      done();
+    }
+  });
+});
+
+describe('makeTickets6180', function () {
+  it('it should make 85 tickets', function(done) {
+    var pt = new PrintTicket('pimaco_6180');
+
+    pt.doc.pipe( fs.createWriteStream('test/results/6180.pdf') );
     pt.doc.fontSize(6);
 
     pt.makeTickets({
