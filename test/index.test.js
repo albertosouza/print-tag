0797e10297b37d19c936396239fd14de7a65fea1
albertosouza <alberto.souza.99@gmail.com>
2019-03-12 14:54:43 -0300

    add pimaco_6081 tests

--- test/index.test.js
+++ test/index.test.js
@@ -84,3 +84,43 @@ describe('makeTickets6180', function () {
     }
   });
 });
+
+describe('makeTickets6081', function () {
+  it('it should make 85 tickets', function(done) {
+    var pt = new PrintTicket('pimaco_6081');
+
+    pt.doc.pipe( fs.createWriteStream('test/results/6081.pdf') );
+    pt.doc.fontSize(6);
+
+    pt.makeTickets({
+      count: data.length
+    }, function (i, marginLeft, marginTop, size, next) {
+
+      pt.doc.text(data[i].name, marginLeft,  marginTop, size)
+      // ticket box
+      pt.doc.lineWidth(0.3);
+
+      pt.doc.rect(marginLeft, marginTop, size.width, size.height).stroke();
+
+      next();
+    }, function() {
+      pt.doc.end();
+    });
+
+    pt.doc.on('end', function() {
+      done();
+    })
+  });
+
+  it('it should throw error if not find the format', function(done) {
+    var p;
+    try {
+      p = new PrintTicket();
+    } catch(e) {
+      assert(!p);
+      assert(e);
+      assert.equal(e.message, 'Unavaible or invalid tag page format');
+      done();
+    }
+  });
+});
